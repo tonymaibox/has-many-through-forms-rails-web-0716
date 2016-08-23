7807b2b05809af891b2fc88a006d1fcbfdf23fbb
Tony Mai <tonymaibox@yahoo.com>
2016-08-23 17:52:54 -0400

    Done.

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
-RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+# RSpec.describe Category, type: :model do
+  # pending "add some examples to (or delete) #{__FILE__}"
+
+   describe 'Category' do
+  	before do
+  		@category = Category.create(name: "Fiction")
+  	end
+
+  it 'has a title' do
+  	expect(@category.name).to eq("Fiction")
+  end
 end
--- spec/models/post_category_spec.rb
+++ spec/models/post_category_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe PostCategory, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+# RSpec.describe PostCategory, type: :model do
+  # pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Post' do
+  	before do
+  		@post = Post.create(title: "Hello", content: "Hello")
+  	end
+
+  it 'has a title' do
+  	expect(@post.title).to eq("Hello")
+  end
 end
